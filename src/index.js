c9dc7cf3a43a0c8db6450cbbdf8974760816109c
VladWithCode <vladwithcode@gmail.com>
2024-09-23 21:29:58 -0600

    add: entrance animations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "./index.css";
+import { gsap } from 'gsap'
 import "./js/AmenitySlider.js"
 
-console.log("Hello world!")
+document.addEventListener('DOMContentLoaded', () => {
+    // Hero animations
+    let heroBgImg = document.getElementById("hero-bg-img")
+    let heroTitle = document.getElementById("hero-title")
+    let heroCta = document.getElementById("hero-cta")
+    let welcoming = document.getElementById("welcoming")
+    let mainTl = gsap.timeline({ ease: "power2.in", duration: 0.4 })
+
+    mainTl.from(heroBgImg, { autoAlpha: 0, y: '5%' })
+    mainTl.from(heroTitle, { autoAlpha: 0, y: '-80%' }, "<0.1")
+    mainTl.to(heroCta, { opacity:1, y: '0%', ease: 'none' }, "<")
+    mainTl.from(welcoming, { opacity: 0, y: '45%' }, ">0.2")
+})
